5c03d205f2373b7e40d9db24127b1013b723c87b
Ritika Saini <ritikasainii1234@gmail.com>
2026-06-16 15:32:14 +0530

    initial changes

--- services/auth/login_wall.py
+++ services/auth/login_wall.py
@@ -9,4 +9,13 @@ def render_login_wall():
     st.markdown("###Welcome! Please enter a username to start.")
 
     with st.form("login_form", clear_on_submit=False):
-        username = 
+        username = st.text_input("Name (unique)", placeholder="unique name e.g Himanshu")
+        submit_button = st.form_submit_button("Start Session", width="stretch")
+
+    if submit_button:
+        if not username:
+            st.error("Name cannot be empty.")
+            return False
+        
+        
+        user = get_or_create_user(username)

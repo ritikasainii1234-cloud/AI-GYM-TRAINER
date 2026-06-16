59461f58ac8393f647c8071d62454bd4f2133dcf
Ritika Saini <ritikasainii1234@gmail.com>
2026-06-16 15:45:19 +0530

    initial changes

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from services.auth.login_wall import render_login_wall
 
-st.set_page_config(page_title="AI Real-time GYM Coach")
+
+def main():
+    st.set_page_config(
+    page_icon="🏋️‍♀️",
+    page_title="AI Real-time GYM Coach",
+    initial_sidebar_state="expanded",
+    layout="centered"
+
+    )
 
 st.title("AI Real-time GYM Coach")
 st.write("Setup done!")
